a32a2f2f0e9ae76d97a77d9bd3734f835d0f9555
lukaarma <lukaarma@gmail.com>
2022-09-05 18:28:45 +0200

    Added User and Vault details
    Co-authored-by: Lorenzo Romio <lorenzo.romio@live.it>

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,20 @@
 <template>
     <v-footer class="pa-0" app absolute>
         <v-card-text class="text-center" width="100%">
-            © {{ new Date().getFullYear() }} — <strong>PassWordManager</strong>
+            <span v-if="userStore.state.authHeader" class="mb-2">
+                Logged as {{ userStore.state.firstName }} | Vault Version
+                {{ vaultStore.state.version }} - Last Modified
+                {{ new Date(vaultStore.state.lastModified).toLocaleString() }}
+            </span><br><!--  FIXME: remove <br> from here -->
+            <span>© {{ new Date().getFullYear() }} — <strong>PassWordManager</strong></span>
         </v-card-text>
     </v-footer>
 </template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/stores/userStore';
+import { useVaultStore } from '@/stores/vaultStore';
+
+const vaultStore = useVaultStore();
+const userStore = useUserStore();
+</script>
